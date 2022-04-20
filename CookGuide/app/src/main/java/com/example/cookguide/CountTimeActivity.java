768669f4d2b0f50ffc40692857952aae5fc453f1
quocluu2k1 <quocluu2k1@gmail.com>
2022-04-21 00:29:17 +0700

    new commit

---
+++ CookGuide/app/src/main/java/com/example/cookguide/CountTimeActivity.java
@@ -0,0 +1,13 @@
+package com.example.cookguide;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CountTimeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
